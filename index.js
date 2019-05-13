6527047cf41198554239f5e32c1eb4bed95afc01
JunYoung <rlawnsdud123123@naver.com>
2019-05-13 21:29:03 +0900

    copy "flyday.co.kr" use html css jquery

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() 
+    {
+        $('.fade , .box1').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        fade: true,
+        autoplaySpeed : 1000,
+        cssEase: 'linear'
+        });
+    });
